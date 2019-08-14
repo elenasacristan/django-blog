34c0f521b40250acaf81d40fd2e0677dc073511e
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-08-14 22:46:00 +0100

    allowed hosts updated in settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','https://project-mini-blog-app.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','project-mini-blog-app.herokuapp.com']
 
 
 # Application definition
